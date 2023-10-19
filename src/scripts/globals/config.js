a8ce4371f91366aa406429ae2093fe10dc154f51
Mifthahuddin <mifthah.huddin24@gmail.com>
2023-10-19 23:46:02 +0800

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '602fce08dc005f3974f41a2651d1996d',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+ 
+export default CONFIG;
