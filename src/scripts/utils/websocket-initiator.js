d383046a0b86cbc0331422d64f41692cd1710969
Mifthahuddin <mifthah.huddin24@gmail.com>
2023-10-20 23:17:16 +0800

    add push notification

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -1,14 +1,15 @@
 import NotificationHelper from './notification-helper';
 import CONFIG from '../globals/config';
-
+ 
 const WebSocketInitiator = {
   init(url) {
     const webSocket = new WebSocket(url);
     webSocket.onmessage = this._onMessageHandler;
   },
-
+ 
   _onMessageHandler(message) {
     const movie = JSON.parse(message.data);
+    
     NotificationHelper.sendNotification({
       title: `${movie.title} is on cinema!`,
       options: {
@@ -18,5 +19,5 @@ const WebSocketInitiator = {
     });
   },
 };
-
+ 
 export default WebSocketInitiator;
